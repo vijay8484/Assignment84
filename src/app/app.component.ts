3e27cb7fc80480f45d6300a645e5778f998c8883
Your NameVijay <vijay8484patil@gmail.com>
2019-01-04 23:36:05 +0530

    Assigment 84

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Assignment81';
+export class AppComponent 
+{
+  sname:string;
+
+  fun(event:any)
+  {
+    this.sname = (<HTMLInputElement>event.target).value;
+  }
 }
